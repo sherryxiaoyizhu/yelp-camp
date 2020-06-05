d162c0391c4188e477dc62f065b18849146d9fec
root <zhuxy9474@gmail.com>
2020-06-05 21:27:14 +0000

    Add reset password feature

--- models/user.js
+++ models/user.js
@@ -5,8 +5,8 @@ var UserSchema = new mongoose.Schema({
 	username: String,
 	password: String,
 	email: {type: String, unique: true, required: true},
-    resetPasswordToken: String,
-    resetPasswordExpires: Date,
+	resetPasswordToken: String,
+	resetPasswordExpires: Date,
 	isAdmin: {type: Boolean, default: false}
 })
 
